bdba8288a3f12bcb870118a0d4559c418d0b134d
Ilia Naryzhny <phantom@ydn.ru>
2023-05-16 06:07:32 -0700

    Readding rel="noreferrer"

--- src/Components/MainComponents/FooterIcon.js
+++ src/Components/MainComponents/FooterIcon.js
@@ -5,16 +5,16 @@ const FooterIcon = () => {
     <div className='social-icons'>
 
 
-  <a href='https://discord.com/channels/@sharonadapp#5857' target="_blank">
+  <a href='https://discord.com/channels/@sharonadapp#5857' target="_blank" rel="noreferrer">
   <i className="bi bi-discord" ></i>
   </a>
-  <a href='https://twitter.com/sharonadapp' target="_blank">
+  <a href='https://twitter.com/sharonadapp' target="_blank" rel="noreferrer">
     <i className="bi bi-twitter"></i>
     </a>
-    <a href='https://www.instagram.com/sharonadapp/' target="_blank">
+    <a href='https://www.instagram.com/sharonadapp/' target="_blank" rel="noreferrer">
     <i className="bi bi-instagram"></i>
     </a>
-    <a href='https://www.facebook.com/SharonaDApp/' target="_blank">
+    <a href='https://www.facebook.com/SharonaDApp/' target="_blank" rel="noreferrer">
     <i class="bi bi-facebook"></i>
     </a>
 
